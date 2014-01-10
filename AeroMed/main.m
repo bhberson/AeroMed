e471bd9fd5a448a06f9fea5077b386ee8cef6e6e
Brody Berson <brody.berson@me.com>
2014-01-10 13:09:20 -0500

    Initial Commit

---
+++ AeroMed/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AeroMed
+//
+//  Created by Brody Berson on 1/10/14.
+//  Copyright (c) 2014 GVSU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ AeroMedTests/AeroMedTests.m
@@ -0,0 +1,34 @@
+//
+//  AeroMedTests.m
+//  AeroMedTests
+//
+//  Created by Brody Berson on 1/10/14.
+//  Copyright (c) 2014 GVSU. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AeroMedTests : XCTestCase
+
+@end
+
+@implementation AeroMedTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
