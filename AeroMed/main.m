49fc5681f855a7e731de3577b965fc312d06f559
Kyle Schelhaas <kyleschelhaas@gmail.com>
2014-02-20 14:02:18 -0500

    Fixed iPad runtime errors
    Pertained to Pixate framework. Also added temporary @try @catch block
    around UIApplicationMain to further investigate errors.

--- AeroMed/main.m
+++ AeroMed/main.m
@@ -15,6 +15,11 @@ int main(int argc, char * argv[])
 {
     @autoreleasepool {
         [Pixate licenseKey:@"Q7JS9-FNOML-SPNJ5-ISE0S-FO9SO-L692A-A480S-LKVON-8UDRP-6FM6R-TVJ1C-VDF5U-7A936-EBV74-M0D0L-42"forUser:@"torresmi@mail.gvsu.edu"];
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        @try {
+            return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        }
+        @catch (NSException * e) {
+            NSLog(@"Exception: %@", e);
+        }
     }
 }
